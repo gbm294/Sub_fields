352bf870fba24c3eded423f5c48de77f1b127717
gbm294 <50755521+gbm294@users.noreply.github.com>
2020-03-03 13:20:40 -0600

    Add files via upload

--- build_use_crosswalk.py
+++ build_use_crosswalk.py
@@ -15,9 +15,9 @@ pd.set_option("display.max_colwidth", 10000)  ##prevents strings from being trun
 pd.set_option("display.max_columns", 100)  ##so all columns will show in print
 
 import csv
-from datetime import datetime
+#from datetime import datetime
 
-import operator
+import numpy as np
 
 
 ### File path to parts ###########################################
@@ -65,34 +65,45 @@ def replace_id(df_in, field_to_sub, new_field_name, cw_path, cnt):
         crosswalk_dict = {}
     
     ### Get highest value in existing dictionary
+#    try:
+#        mx = max(crosswalk_dict.values()) #max(crosswalk_dict, key=crosswalk_dict.get)
+#        cnt = int(mx)
+#    except:
+#        pass
     try:
-        mx = max(crosswalk_dict.values()) #max(crosswalk_dict, key=crosswalk_dict.get)
+        mx = int(max(crosswalk_dict.values()), 36) #max(crosswalk_dict, key=crosswalk_dict.get)
         cnt = int(mx)
     except:
         pass
     
-    
     ## Loop through values in file.  If they exist in dictionary, do nothing. If they don't, then add them.
+    ### may want to pass this to a function #########
+#    for r in df_field_unique:  ## set value to a sequence number
+#        if str(r) in crosswalk_dict:
+#            pass
+#        else:
+#            cnt += 1
+#            crosswalk_dict[str(r)] = str(cnt)
+    cw_changes = 0
+    ###################################################  
     for r in df_field_unique:  ## set value to a sequence number
         if str(r) in crosswalk_dict:
             pass
         else:
             cnt += 1
-            crosswalk_dict[str(r)] = str(cnt)
+            #crosswalk_dict[str(r)] = str(cnt)
+            crosswalk_dict[str(r)] = np.base_repr(cnt, base=36)
+            cw_changes += 1
+    ###################################################  
+    
         
-    
-    
     ind = df_in.columns.get_loc(field_to_sub)  #get index of column
-    df_in[field_to_sub] = df_in[field_to_sub].astype(str)
-    
-    df_in.insert(loc=ind, column=new_field_name, value=df_in[field_to_sub].map(crosswalk_dict))  ##map to new ID
-    
+    df_in[field_to_sub] = df_in[field_to_sub].astype(str)    
+    df_in.insert(loc=ind, column=new_field_name, value=df_in[field_to_sub].map(crosswalk_dict))  ##map to new ID    
     df_subbed = df_in.drop(columns=[field_to_sub])  ##Remove substituted column
     
-    #crosswalk_df = df_in[[field_to_sub,new_field_name]].drop_duplicates().reset_index(drop=True)
-    ##crosswalk_df = pd.DataFrame.from_dict(crosswalk_dict)
     
-    return df_subbed, crosswalk_dict, new_field_name
+    return df_subbed, crosswalk_dict, new_field_name, cw_changes
 ###################################################################
 
 
@@ -110,13 +121,14 @@ def main():
     #print(df_in.head(5))
     
     ### Set fields to replace #################
-    field_list = [{'field_to_sub' : 'MRN'
+    field_list = [
+                {'field_to_sub' : 'MRN'
                    , 'new_field_name' : 'SUBJECT_ID'
                    , 'crosswalk_path' : 'U:/UWHealth/EA/SpecialShares/DM/CRDS/AdHocQueries/Special projects/gm_playground/MRN_crosswalk.csv'
-                   , 'number_to_start' : 10000   ##This should be replaced by a method of substitute
+                   , 'number_to_start' : 1000000000   ##This should be replaced by a method of substitute
                    }
-    
-                    ,{'field_to_sub' : 'ENCOUNTER'
+                    ,
+                    {'field_to_sub' : 'ENCOUNTER'
                       , 'new_field_name' : 'ENCOUNTER_SUB'
                       , 'crosswalk_path' : 'U:/UWHealth/EA/SpecialShares/DM/CRDS/AdHocQueries/Special projects/gm_playground/ENCOUNTER_crosswalk.csv'
                       , 'number_to_start' : 100000  ##This should be replaced by a method of substitute
@@ -133,27 +145,25 @@ def main():
     ###Send to function to substitute field
     for f in field_list:
         ##deident_df, crosswalk_df = replace_id(deident_df, f['field_to_sub'], f['new_field_name'], f['crosswalk_path'], f['number_to_start'])
-        deident_df, crosswalk_dict, new_field_name = replace_id(deident_df, f['field_to_sub'], f['new_field_name'], f['crosswalk_path'], f['number_to_start'])
+        deident_df, crosswalk_dict, new_field_name, cw_changes = replace_id(deident_df, f['field_to_sub'], f['new_field_name'], f['crosswalk_path'], f['number_to_start'])
         
         ## Add crosswalk for field to crosswalks list
         #crosswalks.append({f['field_to_sub']:crosswalk_df})
-        crosswalks.append({f['field_to_sub']:crosswalk_dict})
+        crosswalks.append({f['field_to_sub']:crosswalk_dict, 'changes': cw_changes, 'field_name': f['field_to_sub']} )
     
     
     ### Write outout file
     file_out = '%s_subbed_%s.csv' % (in_file_name, today)  ##build output file name
     deident_df.to_csv( file_out, index=False, encoding='utf-8-sig')
     
-    ### Write crosswalk files
+    ### Write crosswalk files if needed
     for c in crosswalks:
-        for k,v in c.items():
-            file_out = '%s_%s_crosswalk_%s.csv' % (in_file_name, k, today)  ##build output file name
-#            v.to_csv( file_out, index=False, encoding='utf-8-sig')
-
+        if c['changes'] > 0:
+            file_out = '%s_%s_crosswalk_%s.csv' % (in_file_name, c['field_name'], today)  ##build output file name    
             with open(file_out, 'w', newline="") as csv_file:  
                 writer = csv.writer(csv_file)
-                writer.writerow([k,new_field_name])
-                for key, value in c[k].items():
+                writer.writerow([c['field_name'],new_field_name])
+                for key, value in c[c['field_name']].items():
                    writer.writerow([key, value])
                 
     print('Done!')
